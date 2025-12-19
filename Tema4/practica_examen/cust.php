2ec451764793fd4982bf8d7427e8f31898bc6cf2
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-12-19 14:07:55 +0100

    p examen

---
+++ Tema4/practica_examen/cust.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+$_SESSION["CIUDAD"] = $_POST["ciudad"];
+
+#var_dump($_POST);
+
+$dsn = "mysql:host=localhost;dbname=YourStore";
+$user = "root";
+$password = "";
+try {
+    $connection = new PDO($dsn, $user, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT Name,Telephone from customers where City = ?";
+    $statement = $connection->prepare($sql);
+    $ciudad = $_SESSION["CIUDAD"];
+    $statement->bindParam(1,$ciudad);
+    $statement->execute();
+
+
+    echo "<table><caption>CLIENTES DE ".$_SESSION['CIUDAD']."</caption>";
+    echo "<tr> <th>Nombre</th> <th>Telefono</th> </tr>";
+    $customers = $statement->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($customers as $customer){
+    echo "<tr>";
+    echo "<td>",$customer['Name'] , "</td>";
+    echo "<td>",$customer['Telephone'] , "</td>";
+    echo "</tr>";
+    }
+    echo "</table>";
+    
+}catch (PDOException $exception){
+echo "The connection failed. ", $exception->getmessage();
+}
+
+#el 2 es lo mismo, paso de hacerlo
+?>
---
+++ Tema4/practica_examen/pForm.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset='utf-8'>
+        <title>Seleccion de ciudad</title>
+    </head>
+    <body>
+        <?php
+            $dsn = "mysql:host=localhost;dbname=YourStore";
+            $user = "root";
+            $password = "";
+            try {
+                $connection = new PDO($dsn, $user, $password);
+                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = "SELECT distinct City FROM customers ORDER BY City asc";
+                $statement = $connection->query($sql);
+                echo "<h1>Listado de ciudades</h1>";
+                echo "<form action='cust.php' method='POST'>
+                        <fieldset>
+                            <select name='ciudad'>";
+                             foreach ($statement->fetchAll(PDO::FETCH_COLUMN) as $city){
+                                echo "<option value='$city'>$city</option>";
+                            }
+                            echo "</select>
+                        </fieldset>
+                        <input type='submit' value='Enviar'/>
+                     </form>"; 
+            }catch (PDOException $exception){
+            echo "The connection failed. ", $exception->getmessage();
+            }
+            $connection = null;
+        ?>   
+    </body>
+</html>

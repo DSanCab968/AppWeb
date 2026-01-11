a90088c78265c0c312cd29147798a2f0707f0556
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2026-01-11 20:09:05 +0100

    ej del provider acabado

--- Tema4/practica_examen/provider part/pForm.php
+++ Tema4/practica_examen/provider part/pForm.php
@@ -12,14 +12,15 @@
             try {
                 $connection = new PDO($dsn, $user, $password);
                 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $sql = "SELECT distinct Name FROM providers ORDER BY Name asc";
+                $sql = "SELECT Name FROM providers ORDER BY Name asc";
                 $statement = $connection->query($sql);
                 echo "<h1>Listado de proveedores</h1>";
                 echo "<form action='prov.php' method='POST'>
                         <fieldset>
                             <select name='nombre'>";
                              foreach ($statement->fetchAll(PDO::FETCH_COLUMN) as $name){
-                                echo "<option value='$name'>$name</option>";
+                                $safeName = htmlspecialchars($name);
+                                echo "<option value='$safeName'>$safeName</option>";
                             }
                             echo "</select>
                         </fieldset>
--- Tema4/practica_examen/provider part/prov.php
+++ Tema4/practica_examen/provider part/prov.php
@@ -11,9 +11,11 @@ $password = "";
 try {
     $connection = new PDO($dsn, $user, $password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT Name from products 
-    inner join providers on 
-    where ProviderId = ?";
+    $sql = "SELECT products.Name from products 
+    inner join providers on products.Provider = providers.ProviderId
+    where providers.Name = ?
+    order by products.Name asc";
+
     $statement = $connection->prepare($sql);
     $nombre = $_SESSION["NOMBRE"];
     $statement->bindParam(1,$nombre);
@@ -21,10 +23,10 @@ try {
 
 
     echo "<h3>PRODUCTOS DEL PROVEEDOR ".$_SESSION['NOMBRE'];
-    echo "<ul";
+    echo "<ul>";
     $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     foreach ($products as $product){
-    echo "<li>",$product['Name'] , "</li>";
+    echo "<li>".htmlspecialchars($product['Name']). "</li>";
     }
     echo "</ul>";
     
